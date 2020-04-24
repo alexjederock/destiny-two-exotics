5b81d704984e7fc50517db971eaf8ad45e42fcda
alexjederock <60864509+alexjederock@users.noreply.github.com>
2020-04-24 16:26:39 -0400

    building up Rest API, just a dataset and some html/css so far.

---
+++ hunters.js
@@ -0,0 +1,49 @@
+const hunters = [ {
+  name: 'Knucklehead Radar',
+  type: 'Helmet',
+  perk: 'Upgraded Sensor Pack; Provides radar while your aiming, enhanced radar while crouching.'
+}, {
+  name: 'Celestial Nighthawk',
+  type: 'Helmet',
+  perk: 'Hawkeye Hack; Golden Gun fires one high-damage shot.'
+}, {
+  name: 'Wormhusk Crown',
+  type: 'Helmet',
+  perk: 'Burning Souls; Dodging gives a small health and shield bump.'
+}, {
+  name: 'Young Ahamkaras Spine',
+  type: 'Gauntlets',
+  perk: 'Wish-Dragon Teeth; Ability damage grants enhanced Tripmines.'
+}, {
+  name: 'Shards of Galanor',
+  type: 'Gauntlets',
+  perk: 'Sharp Edges; Get Super energy for Blade Barrage hits and kills.'
+}, {
+  name: 'Liars Handshake',
+  type: 'Gauntlets',
+  perk: 'Cross-Counter; Cross Counter regenerates health and deals etra damage.'
+}, {
+  name: 'Ophidia Spathe',
+  type: 'Chest Armor',
+  perk: 'Scissor-fingers; Grants two knives per charge.'
+}, {
+  name: 'Gwisin Vest',
+  type: 'Chest Armor',
+  perk: 'Roving Assassin; Vanish after Spectral Blades kills for more Super Energy.'
+}, {
+  name: 'The Sixth Coyote',
+  type: 'Chest Armor',
+  perk: 'Double Dodge; Gain a second didge charge.'
+}, {
+  name: 'Stomp-EE5',
+  type: 'Leg Armor',
+  perk: 'Hydraulic Boosters; Buffs sprint, slide, and jumping.'
+}, {
+  name: 'Orpheus Rig',
+  type: 'Leg Armor',
+  perk: 'Uncanny Arrows; Grants Deadfall and Moebius Quiver energy.'
+}, {
+  name: 'Lucky Pants',
+  type: 'Leg Armor',
+  perk: 'Illegally Modded Holster; Precision hits load a round to stowed Hand Cannons'
+} ]
